fcd3322ec47b73594d0c988376456e6fb54d4d0f
Spencer Roh <spencer.roh@gmail.com>
2015-08-29 19:12:08 +0900

    관리자 모드도 JWT 지원하도록 수정함.

---
+++ admin/src/app/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 영태 on 2015-08-29.
+ */
+angular.module('undercity')
+    .constant('SERVICE_ENDPOINT', 'http://localhost/undercity/api')
+    .constant('IMAGE_ENDPOINT', 'http://localhost/undercity/api/images/');
+//.constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
+//.constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/')
