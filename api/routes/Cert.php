ab6ac47e198900c73eeac473cd8aebef370d52bb
Spencer Roh <spencer.roh@gmail.com>
2015-08-29 18:20:17 +0900

    인증서 위치 변경

--- api/routes/Cert.php
+++ api/routes/Cert.php
@@ -6,6 +6,6 @@
 
 $app->group('/cert', function () use ($app) {
     $app->get('/', function () use ($app) {
-        echo file_get_contents('../certs/public.pem');
+        echo file_get_contents('certs/public.pem');
     });
 });
